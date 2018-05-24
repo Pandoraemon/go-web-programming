3814bc041c956b354ec86e60cd9d868312046576
pandoraemon <pz880304@163.com>
2018-05-24 18:00:07 +0800

    4-6

--- chapter04/4-6/server.go
+++ chapter04/4-6/server.go
@@ -3,10 +3,19 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"io/ioutil"
 )
 
 func process(w http.ResponseWriter, r *http.Request) {
 	r.ParseMultipartForm(1024)
+	fileHeader := r.MultipartForm.File["uploaded"][0]
+	file, err := fileHeader.Open()
+	if err == nil {
+		data, err := ioutil.ReadAll(file)
+		if err == nil {
+			fmt.Fprintln(w, string(data))
+		}
+	}
 	fmt.Fprintln(w, r.MultipartForm)
 }
 
