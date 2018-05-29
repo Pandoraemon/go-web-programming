eb8308cdf991a378907cfccdc78955ddf9cc4a14
pandoraemon <pz880304@163.com>
2018-05-29 16:15:32 +0800

    add test

---
+++ chapter09/9-7/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func printNumbers1() {
+	for i := 0; i < 10; i++ {
+		time.Sleep(1 * time.Microsecond)
+		fmt.Printf("%d ", i)
+	}
+}
+
+func printLetters1() {
+	for i := 'A'; i < 'A' + 10; i++ {
+		time.Sleep(1 * time.Microsecond)
+		fmt.Printf("%c ", i)
+	}
+}
+
+func printNumbers2(w chan bool) {
+	for i := 0; i < 10; i++ {
+		time.Sleep(1 * time.Microsecond)
+		fmt.Printf("%d ", i)
+	}
+	w <- true
+}
+
+func printLetters2(w chan bool) {
+	for i := 'A'; i < 'A' + 10; i++ {
+		time.Sleep(1 * time.Microsecond)
+		fmt.Printf("%c ", i)
+	}
+	w <- true
+}
+
+func print1() {
+	printNumbers1()
+	printLetters1()
+}
+
+func goPrint1() {
+	go printNumbers1()
+	go printLetters1()
+}
+
+func main() {
+	w1, w2 := make(chan bool), make(chan bool)
+	go printNumbers2(w1)
+	go printLetters2(w2)
+	<-w1
+	<-w2
+}
